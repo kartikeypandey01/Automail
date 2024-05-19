0f61ff705ebc15e941f36f56360e8ac2076cf483
kartikey pandey <kpandey0601@gmail.com>
2024-05-19 21:39:54 +0530

    Initial commit

--- mail.sh
+++ mail.sh
@@ -1,5 +1,3 @@
 echo mail.sh called: `date` > /mnt/c/Users/user/onedrive/desktop/email_script/cronlog.txt
-#HOME = /mnt/c/Users/user/onedrive/desktop/email_script
-#PYTHONPATH = /home/kp/.local/lib/python310/site-packages
 cd /mnt/c/Users/user/onedrive/desktop/email_script
 /usr/bin/python3 /mnt/c/Users/user/onedrive/desktop/email_script/mail_script.py > /mnt/c/Users/user/onedrive/desktop/email_script/cron_log.log 2>&1
--- mail_script.py
+++ mail_script.py
@@ -26,7 +26,7 @@ def get_credentials():
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file('credentials.json',SCOPES)
-            creds = flow.run_local_server(host='localhost',port=8080,open_browser=True)
+            creds = flow.run_local_server(port=0)
         with open('token.json','w') as token:
             token.write(creds.to_json())
     return creds
